cfa66564883dfb02f301cd127b0b708a1146e97c
CHENRight <reerchen@163.com>
2019-08-19 21:07:30 +0800

    Refactor,operate different by item's name.

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -8,62 +8,47 @@ class GildedRose {
 
     void updateQuality() {
         for (Item item : items) {
-            //不是Aged ，也不是 back
-            if (!isNameEqualAGEDorBackstage(item)) {
-                if (item.quality > 0) {
-                    //不是Sulf
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            }
-            //是aged 或是 back
-            else {
+            if (item.name.equals("Aged Brie")) {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
-
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
                 }
-            }
-
-            //除了 Sulfur都减1
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                 item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.sellIn < 0) {
-                //除了 Aged
-                if (!item.name.equals("Aged Brie")) {
-                    //除了Back
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
-                            //三者均不是
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    }
-                    //Backstage
-                    else {
-                        item.quality = 0;
-                    }
-                }
-                //Aged
-                else {
+                if (item.sellIn < 0) {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
                     }
                 }
+            } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                return;
+            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+                item.sellIn = item.sellIn - 1;
+                if (item.sellIn < 0) {
+                    item.quality = 0;
+                }
+            } else {
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                }
+                item.sellIn = item.sellIn - 1;
+                if (item.sellIn < 0) {
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1;
+                    }
+                }
             }
         }
-    }
-    private boolean isNameEqualAGEDorBackstage(Item item){
-        return item.name.equals("Aged Brie") || item.name.equals("Backstage passes to a TAFKAL80ETC concert");
     }
 }
